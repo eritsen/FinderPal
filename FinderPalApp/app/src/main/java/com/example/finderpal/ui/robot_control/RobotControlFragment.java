8b84d04e6c1e449da664c300d2c8c65a8a13e42e
Nathan <nzpancakes@gmail.com>
2023-12-05 17:27:49 -0500

    Activity Logs Screen

--- FinderPalApp/app/src/main/java/com/example/finderpal/ui/robot_control/RobotControlFragment.java
+++ FinderPalApp/app/src/main/java/com/example/finderpal/ui/robot_control/RobotControlFragment.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.finderpal.R;
 import com.example.finderpal.databinding.FragmentRobotControlBinding;
 
 public class RobotControlFragment extends Fragment {
@@ -24,8 +26,69 @@ public class RobotControlFragment extends Fragment {
         binding = FragmentRobotControlBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textRobotcontrol;
-        robotcontrolViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        Button manualControlDisableButton = root.findViewById(R.id.manualcontroldisablebutton);
+        Button manualControlEnableButton = root.findViewById(R.id.manualcontrolenablebutton);
+
+        Button forwardButton = root.findViewById(R.id.arrowforwardbutton);
+        Button backwardsButton = root.findViewById(R.id.arrowbackwardsbutton);
+        Button leftButton = root.findViewById(R.id.arrowleftbutton);
+        Button rightButton = root.findViewById(R.id.arrowrightbutton);
+
+        forwardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                RobotControlViewModel.onForwardButtonClick();
+            }
+        });
+
+        backwardsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                //
+                RobotControlViewModel.onBackwardButtonClick();
+            }
+        });
+
+        leftButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                //
+                RobotControlViewModel.onLeftButtonClick();
+            }
+        });
+
+        rightButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                //
+                RobotControlViewModel.onRightButtonClick();
+            }
+        });
+
+        manualControlDisableButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                manualControlEnableButton.setEnabled(true);
+                RobotControlViewModel.onManualControlDisableButtonClick();
+                manualControlDisableButton.setEnabled(false);
+            }
+        });
+
+        manualControlEnableButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                manualControlDisableButton.setEnabled(true);
+                RobotControlViewModel.onManualControlEnableButtonClick();
+                manualControlEnableButton.setEnabled(false);
+            }
+        });
+
         return root;
     }
 
--- FinderPalApp/app/src/main/java/com/example/finderpal/ui/robot_control/RobotControlViewModel.java
+++ FinderPalApp/app/src/main/java/com/example/finderpal/ui/robot_control/RobotControlViewModel.java
@@ -1,8 +1,20 @@
 package com.example.finderpal.ui.robot_control;
 
+import android.util.Log;
+import android.view.View;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.example.finderpal.R;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 
 public class RobotControlViewModel extends ViewModel {
 
@@ -12,6 +24,82 @@ public class RobotControlViewModel extends ViewModel {
         mText = new MutableLiveData<>();
         mText.setValue("This is the robot control fragment");
     }
+
+    public static void onForwardButtonClick()
+    {
+
+        Log.i("Forward button","forward button pressed");
+    }
+    public static void onBackwardButtonClick()
+    {
+        Log.i("Backward button","backward button pressed");
+    }
+
+    public static void onRightButtonClick()
+    {
+        Log.i("Right button","right button pressed");
+    }
+    public static void onLeftButtonClick()
+    {
+        Log.i("Left button","left button pressed");
+    }
+
+    public static void onManualControlEnableButtonClick()
+    {
+        Log.i("Manual Control Enable button","Manual Control Enable button pressed");
+    }
+
+    public static void onManualControlDisableButtonClick()
+    {
+        Log.i("Manual Control Disable button","Manual Control Disable button pressed");
+    }
+
+    public static void doPutRequest()
+    {
+        //performs http put request to send data to the /resource endpoint on the localhost server
+        URL url = null;
+        try {
+            url = new URL("http://localhost/resource");
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+        HttpURLConnection httpCon = null;
+        try {
+            httpCon = (HttpURLConnection) url.openConnection();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        httpCon.setDoOutput(true);
+        try {
+            httpCon.setRequestMethod("PUT");
+        } catch (ProtocolException e) {
+            throw new RuntimeException(e);
+        }
+        OutputStreamWriter out = null;
+        try {
+            out = new OutputStreamWriter(
+                    httpCon.getOutputStream());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            out.write("Resource content");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            out.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            httpCon.getInputStream();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
 
     public LiveData<String> getText() {
         return mText;
